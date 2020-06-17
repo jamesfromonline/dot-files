17db25b0865e1560991ecb098b071acdae22b147
james <jtwebco@gmail.com>
2020-06-17 17:09:34 -0400

    updates

--- .hyper.js
+++ .hyper.js
@@ -1,20 +1,27 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
 module.exports = {
-  updateChannel: "canary",
+  updateChannel: 'canary',
   config: {
     // default font size in pixels for all tabs
     fontSize: 20,
     windowSize: [1080, 720],
     fontFamily: '"Fira Code", "Inconsolata for Powerline", monospace',
-    cursorShape: "BLOCK",
+    cursorShape: 'BLOCK',
     wickedBorder: true,
-    padding: "10px",
-    shell: "/bin/zsh"
+    padding: '10px',
+    shell: '/bin/zsh'
   },
 
-  plugins: ["hyperterm-cobalt2-theme"]
+
+  plugins: [
+    'hyperterm-cobalt2-theme',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
   // localPlugins: ['hyperterm-cobalt2-theme'],
-}
+};
--- .zshrc
+++ .zshrc
@@ -1,11 +1,13 @@
 export ZSH="/Users/jamesobrien/.oh-my-zsh"
+export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
 
 ZSH_THEME="cobalt2"
 plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
-
+alias darnell="ssh root@167.99.121.93"
+alias home="ssh james@192.168.0.5"
 alias slack="open -a slack"
 alias spotify="open -a spotify"
 alias chrome="open -a chrome"
